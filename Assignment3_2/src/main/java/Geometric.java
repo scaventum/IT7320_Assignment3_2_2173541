36bc526a719526a7039e6ec43116200cda9c62e9
Ryan.Djoenaidi <Ryan.Djoenaidi@B204-05.soit.local>
2017-10-02 12:30:34 +1300

    First Commit

---
+++ Assignment3_2/src/main/java/Geometric.java
@@ -0,0 +1,23 @@
+
+public class Geometric {
+
+	public double diameter(int r) {
+		double result = 0.0;
+		result = 2 * r;
+		return result;
+	}
+	
+	public double circlePerimeter(int r) {
+		double result = 0.0;
+		result = (22  * 2 * r) / 7;
+		return result;
+	}
+
+
+	public double circleArea(int r) {
+		double result = 0.0;
+		result = (22 * r * r)/ 7;
+		return result;
+	}
+	
+}
---
+++ Assignment3_2/src/test/java/GeometricTest.java
@@ -0,0 +1,37 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GeometricTest {
+	Geometric obj;
+	int r;
+	@Before
+	public void setUp() throws Exception {
+		obj = new Geometric();
+		r = 14;
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		obj = null;
+		r = 0;
+	}
+
+	@Test
+	public void testDiameter() {
+		assertEquals(28.0,obj.diameter(r),0.1);
+	}
+
+	@Test
+	public void testCirclePerimeter() {
+		assertEquals(88.0,obj.circlePerimeter(r),0.1);
+	}
+
+	@Test
+	public void testCircleArea() {
+		assertEquals(616.0,obj.circleArea(r),0.1);
+	}
+
+}
