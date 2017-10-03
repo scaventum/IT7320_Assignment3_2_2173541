2c320a2f46297075d38ed850e7c39cd810c630e4
scaventum <30420314+scaventum@users.noreply.github.com>
2017-10-04 10:55:02 +1300

    Add comments

--- Assignment3_2/src/main/java/Geometric.java
+++ Assignment3_2/src/main/java/Geometric.java
@@ -1,18 +1,21 @@
 
 public class Geometric {
-
+	
+	//measuring diameter of a circle with radius
 	public double circleDiameter(int r) {
 		double result = 0.0;
 		result = 2 * r;
 		return result;
 	}
 	
+	//measuring perimeter of a circle with radius
 	public double circlePerimeter(int r) {
 		double result = 0.0;
 		result = (22  * 2 * r) / 7;
 		return result;
 	}
 
+	//measuring area of a circle with radius
 	public double circleArea(int r) {
 		double result = 0.0;
 		result = (22 * r * r)/ 7;
